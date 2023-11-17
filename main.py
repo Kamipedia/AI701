8819ccdb3f79e8f819301d36258fd2f25718c204
Vladislav <91109627+VGMitkin@users.noreply.github.com>
2023-11-17 15:17:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -14,48 +14,54 @@ import random
 import gc
 from glob import glob
 from tqdm import tqdm
-from model import *
-from dataset import *
-from train_loop import *
+from model import *       # Importing model definition from 'model.py'
+from dataset import *     # Importing dataset class from 'dataset.py'
+from train_loop import *  # Importing training loop from 'train_loop.py'
 
-
-DEVICE='cuda:3'
-TASK="fvit_unet_softmax_bce_loss"
-LR=1e-3
+DEVICE = 'cuda:3'
+TASK = "fvit_unet_softmax_bce_loss"
+LR = 1e-3
 IMG_SIZE = 512
 EPOCHS = 50
 VAL_EPOCHS = 1
 BATCH_SIZE = 32
-TOLERANCE=5
-TOL_THRESHOLD=0.01
+TOLERANCE = 5
+TOL_THRESHOLD = 0.01
 
+START_seed()  # Set random seeds for reproducibility
 
-START_seed()
-
+# Create training dataset and dataloader
 train_ds = DatasetClass(data='/share/sda/nurenzhaksylyk/maxim.popov/data/submission/syntax/train/images/*', 
                         target='/share/sda/nurenzhaksylyk/maxim.popov/data/miccai_binary/syntax/train/images/*'
                         )
 train_loader = DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True, num_workers=24)
 
+# Create test dataset and dataloader
 test_ds = DatasetClass(data='/share/sda/nurenzhaksylyk/maxim.popov/data/submission/syntax/test/images/*', 
                        target='/share/sda/nurenzhaksylyk/maxim.popov/data/miccai_binary/syntax/test/images/*'
                        )
 test_loader = DataLoader(test_ds, batch_size=BATCH_SIZE, shuffle=False, num_workers=24)
 
-
+# Create UNet model
 model = Unet(in_channels=1, out_channels=2)
 model = model.to(DEVICE)
 
+# Define optimizer, loss criterion, and learning rate scheduler
 optimizer = AdamW(model.parameters(), lr=LR)
-criterion = nn.BCELoss()#nn.CrossEntropyLoss()
+criterion = nn.BCELoss()  # Binary Cross-Entropy Loss
+# Alternatively, use the following for multi-class segmentation:
+# criterion = nn.CrossEntropyLoss()
+
 scheduler = CosineAnnealingLR(optimizer, 10, 0, -1, verbose=False)
 
+# Load pre-trained weights if available
 if os.path.isfile(f'best_{TASK}.pth'):
     model.load_state_dict(torch.load(f'best_{TASK}.pth'))
-    print('Weights has been preloaded')
-
+    print('Weights have been preloaded')
 
 torch.autograd.set_detect_anomaly(True)
+
+# Train the model using the training loop
 hist_base = train(model, train_loader, 
                   test_loader, 
                   optimizer, 
